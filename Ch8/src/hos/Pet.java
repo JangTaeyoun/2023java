09296b21acdf4ba1e6d341a672a3587c4e36d352
JangTaeYoung <147689082+JangTaeyoun@users.noreply.github.com>
2023-11-07 09:07:36 +0900

    Ch8

---
+++ Ch8/src/hos/Pet.java
@@ -0,0 +1,14 @@
+package hos;
+
+public interface Pet {
+	//protected String owner;
+	
+	
+		//public Pet(String owner)
+		//{
+		//	this.owner = owner;
+		//}
+		
+		String OwnerName();
+		void Cry();
+}
